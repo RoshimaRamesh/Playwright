5d3039359349727cf97a57dae3389353e0a412b5
RoshimaRamesh <101967559+RoshimaRamesh@users.noreply.github.com>
2024-04-27 17:45:43 +0530

    Add files via upload

---
+++ primitive_datatypes.js
@@ -0,0 +1,16 @@
+const { clear } = require("console");
+
+let firstName = "Roshima"
+console.log(typeof firstName);
+
+let companyName = "HCL"
+console.log(typeof companyName);
+
+let mobileNumber = 949803172;
+console.log(typeof mobileNumber);
+
+let isAutomation = true;
+console.log(typeof isAutomation);
+
+let hasPlaywright ;
+console.log(typeof hasPlaywright);
