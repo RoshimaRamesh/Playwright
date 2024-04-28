378edab881fbb01022b1bab2ec4a6af99252cef6
RoshimaRamesh <101967559+RoshimaRamesh@users.noreply.github.com>
2024-04-28 09:00:12 +0530

    Add files via upload

---
+++ variable_scoping.js
@@ -0,0 +1,16 @@
+
+const browserVersion = 'Chrome1';
+
+function getBrowserVersion() {
+
+    if(browserVersion == 'Chrome1')
+    {
+        //var browserVersion = 'Chrome2'
+        let browserVersion = 'Chrome2';
+    }
+
+        console.log(browserVersion);
+
+}
+
+getBrowserVersion();
